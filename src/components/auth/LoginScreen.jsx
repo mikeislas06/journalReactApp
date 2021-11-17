0d7954a4373d12026d3cbe6ea9dcd85290dbea38
Miguel Islas <isgromiguel@gmail.com>
2021-11-17 13:33:58 -0600

    cleaning login fields

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -13,8 +13,8 @@ const LoginScreen = () => {
   const { loading } = useSelector((state) => state.ui);
 
   const [formValues, handleInputChange] = useForm({
-    email: 'miguel@gmail.com',
-    password: '123456',
+    email: '',
+    password: '',
   });
 
   const { email, password } = formValues;
