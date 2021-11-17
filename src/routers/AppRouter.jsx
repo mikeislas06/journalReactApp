08a81c12b5c68eba840f387ab3926b0a676de0b3
Miguel Islas <isgromiguel@gmail.com>
2021-11-17 17:38:32 -0600

    AppRouter BrowserRouter change to HashRouter

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Switch, Redirect } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
 // Router
